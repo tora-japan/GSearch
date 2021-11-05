79d67d506f6a443b695459876a0706d3dd71ae53
noda <i8086jp@yahoo.co.jp>
2021-11-05 09:15:32 +0900

    '地図検索の機能を追加'

--- background.js
+++ background.js
@@ -9,20 +9,35 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.sendMessage(tab.id, ["Action"]);
 });
 
+/*
+テストコード
 chrome.runtime.onInstalled.addListener(() => {
+  const parent = chrome.contextMenus.create({
+      contexts:["selection"],   // ← ※ここ重要(選択したときにコンテキストメニューを作る)
+      id: 'parent',
+      title: '選択範囲の単語を画像検索',
+      onclick: function(info, tab) {
+        chrome.tabs.sendMessage(tab.id, ["Action1",info.selectionText]);
+      }
+  });
+}
+*/
+
+chrome.runtime.onInstalled.addListener(() => {
+
   //
   // 選択したときにコンテキストメニューを作り、クリック時にメッセージを投げる
   //
   const parent = chrome.contextMenus.create({
-      contexts:["selection"],   // ← ※ここ重要(選択したときにコンテキストメニューを作る)
-      id: 'parent',
-      title: '選択範囲を検索'
+    contexts:["selection"],   // ← ※ここ重要(選択したときにコンテキストメニューを作る)
+    id: 'parent',
+    title: '選択した単語を検索'
   });
   chrome.contextMenus.create({
     contexts:["selection"],   // ← ※子でもこれが無いと表示されない
     id: 'child1',
     parentId: 'parent',
-    title: '選択範囲の単語を画像検索',
+    title: '画像検索',
     onclick: function(info, tab) {
       chrome.tabs.sendMessage(tab.id, ["Action1",info.selectionText]);
     }
@@ -31,9 +46,18 @@ chrome.runtime.onInstalled.addListener(() => {
     contexts:["selection"],   // ← ※子でもこれが無いと表示されない
     id: 'child2',
     parentId: 'parent',
-    title: '選択範囲の単語を動画検索',
+    title: '動画検索',
     onclick: function(info, tab) {
       chrome.tabs.sendMessage(tab.id, ["Action2",info.selectionText]);
+    }
+  });
+  chrome.contextMenus.create({
+    contexts:["selection"],   // ← ※子でもこれが無いと表示されない
+    id: 'child3',
+    parentId: 'parent',
+    title: '地図検索',
+    onclick: function(info, tab) {
+      chrome.tabs.sendMessage(tab.id, ["Action3",info.selectionText]);
     }
   });
 });
--- script.js
+++ script.js
@@ -13,33 +13,49 @@
     >> https://www.google.co.jp/search?hl=ja&tbm=isch&q=%E8%8A%B1
 */
 
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse)
+{
   console.log("メッセージ",request[0]);
-  if (request[0] == "Action") {
+  if (request[0] == "Action")
+  {
     // 選択範囲の単語を読み込む
     var selectedText = window.getSelection().toString();
     gSearch(selectedText,'isch');  // 画像検索
-  }
-  if (request[0] == "Action1") {
+  }else
+  if (request[0] == "Action1")
+  {
     gSearch(request[1],'isch');  // 画像検索
-  }
-  if (request[0] == "Action2") {
+  }else
+  if (request[0] == "Action2")
+  {
     gSearch(request[1],'vid');   // 動画検索
+  }else
+  if (request[0] == "Action3")
+  {
+    gSearch(request[1],'map');   // 地図検索
   }
 });
 
 //
-// 新しいウインドウで、単語をgoogleで画像検索をする
+// 新しいウインドウで、単語をgoogleで検索をする
 //
 function gSearch(selectedText,kind) {
 //  console.log('選択範囲',selectedText);
 
   if(selectedText!="")
   {
-    var url= 'https://www.google.co.jp/search?hl=ja&tbm='+kind+'&q='+selectedText;
+    var url;
+    if('map' == kind)
+    {
+      url= 'https://www.google.co.jp/maps?q='+selectedText;
+    }else
+    {
+      url= 'https://www.google.co.jp/search?hl=ja&tbm='+kind+'&q='+selectedText;
+    }
     var encodeUrl = encodeURI(url);
 //    console.log(url);
 //    console.log(encodeUrl);
     window.open(encodeUrl);
   }
 }
+
